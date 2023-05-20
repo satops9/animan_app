71e46dbd2dff68d51396b4baf076fdb00ba3f427
satops9 <zenjirowhr@gmail.com>
2023-05-20 20:20:56 +0900

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
 <html class="h-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <link rel="icon" href="https://zatops.sakura.ne.jp/animan_apps/bkm/animan.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
